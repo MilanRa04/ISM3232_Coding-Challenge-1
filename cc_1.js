7d6add7c2529f6d13677f5fcd5dc459a0d2d6c28
Milan Ranpariya <ranpariya04@gmail.com>
2024-09-01 02:24:28 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,19 @@
+// Declare variable employeeName using let and assign it the employee's name as a string
+let employeeName = 'Bob Henry';
+
+// Declare variable employeeID using const and assing it the employee's ID as a number
+const employeeID = 49857;
+
+// Declare variable isActive using var and assign it a boolean value true to indicate the employee is currently active
+var isActive = true;
+
+// Log each variable's value and its type to the console.
+
+console.log('employeeName:', employeeName);
+console.log('Type of employeeName:', typeof employeeName);
+
+console.log('employeeID:', employeeID);
+console.log('Type of employeeID:', typeof employeeID);
+
+console.log('is Active:', isActive);
+console.log('Type of is Active:', typeof isActive);
